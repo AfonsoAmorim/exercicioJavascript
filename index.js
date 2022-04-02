62724eb3e3939421f8a364ddc392e9176448e307
Afonso Gomes de Amorim <afonso.amorim2014@hotmail.com>
2022-04-02 17:54:07 -0300

    new update

--- index.js
+++ index.js
@@ -2,24 +2,50 @@
 class Cliente{
     nome;
     cpf;
-    agencia;
-    saldo;   
+     
 }
 
+class ContaCorrente{
+    agencia;
+    saldo = 0;
+
+    sacar(valor){
+        if(this.saldo >= valor){
+            this.saldo -= valor;
+            return valor;
+        }
+    }
+    depositar(valor){
+        if(valor <= 0)
+        {
+            return;
+        } 
+        this.saldo += valor;           
+    }
+}
+
+
+
 const cliente1 = new Cliente();
-const cliente2 = new Cliente();
-
-
 cliente1.nome = "Alfonso";
 cliente1.cpf = 22222222222;
-cliente1.agencia = 4444;
-cliente1.saldo = 2252;
 
+const cliente2 = new Cliente();
 cliente2.nome = "Aline";
 cliente2.cpf = 8885522222;
-cliente2.agencia = 3354;
-cliente2.saldo = 50;
+
+const ContaCorrenteAlfonso = new ContaCorrente();
+ContaCorrenteAlfonso.agencia = 5522;
+
+
+
+ContaCorrenteAlfonso.depositar(2000);
+
+const valorSacado = ContaCorrenteAlfonso.sacar(300);
+
 
 console.log(cliente1)
 console.log(cliente2)
 
+console.log("O saldo e agência do Alfonso é:  ");
+console.log(ContaCorrenteAlfonso)
