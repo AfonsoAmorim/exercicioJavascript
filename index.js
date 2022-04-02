43848c106e6207172d729365d01a1e9ca014593e
Afonso Gomes de Amorim <afonso.amorim2014@hotmail.com>
2022-04-02 17:27:21 -0300

    classe Javascript

---
+++ index.js
@@ -0,0 +1,25 @@
+
+class Cliente{
+    nome;
+    cpf;
+    agencia;
+    saldo;   
+}
+
+const cliente1 = new Cliente();
+const cliente2 = new Cliente();
+
+
+cliente1.nome = "Alfonso";
+cliente1.cpf = 22222222222;
+cliente1.agencia = 4444;
+cliente1.saldo = 2252;
+
+cliente2.nome = "Aline";
+cliente2.cpf = 8885522222;
+cliente2.agencia = 3354;
+cliente2.saldo = 50;
+
+console.log(cliente1)
+console.log(cliente2)
+
